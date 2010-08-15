6eff7ceb9c7d5aa68ec9a2f2d829c0708b45b80c
Brad Huber <badashbrad@yahoo.com>
2010-08-14 19:33:17 -0500

    Better select dropdown

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,7 @@ class Company < ActiveRecord::Base
   has_many :projects
   accepts_nested_attributes_for :projects, :allow_destroy => true
   
+  def exists?
+    self && self.id
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,36 @@
-$(document).ready(function() {
+$(function() {	
+	$('#select_company').find('.m_itemMain').toggle(
+		function(){
+			var $this = $(this);
+			$this.addClass('m_down').removeClass('m_up');
+			var $menu = $this.prev();
+			$($menu.find('li').get().reverse()).each(function(){
+				$(this).show();
+			});
+			$menu.show('600');
+		},
+		function(){
+			var $this 	= $(this);
+			$this.addClass('m_up').removeClass('m_down');
+			var $menu	= $this.prev();
+			$menu.hide('1000', function() {
+				$($menu.find('li').get().reverse()).each(function(){
+					var $li = $(this);
+					$li.hide();
+				});
+			});
+		}
+	);
 	
-	$("#choose_company").change(function(){
-		$("#project_list").load("/companies/" + $(this).val() + "/projects");
-	})
-	
-})
+	$(".available_company").click(function(){
+		$('.m_itemMain').click();
+		comp_id = $(this).attr('id');
+		$("#selected_company").html($(this).html());
+		if (comp_id == "new"){
+			
+		} else{
+			temp = comp_id.split('_');
+			$("#project_list").load("/companies/" + temp[2] + "/projects");
+		}
+	});
+});
